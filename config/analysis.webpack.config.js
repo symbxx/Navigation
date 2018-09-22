8d1b44fa5bc4ea9df0c41814c99dd3a1f81f4ab6
symbxx <symb@21cn.com>
2018-09-22 10:46:39 +0800

    删除项目内容

--- config/analysis.webpack.config.js
+++
@@ -1,7 +0,0 @@
-const config = require('./production.webpack.config');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
-	.BundleAnalyzerPlugin;
-
-config.plugins.push(new BundleAnalyzerPlugin());
-
-module.exports = config;
--- test/unit/index.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue';
-import ElementUI from 'element-ui';
-Vue.use(ElementUI);
-
-const testsContext = require.context('./specs', true, /\.spec$/);
-
-
-testsContext.keys().forEach(testsContext);
